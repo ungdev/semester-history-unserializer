03144f9ba258c84ce3d4229662f75998f414bb57
Ivann LARUELLE <ivann+git@laruelle.me>
2021-08-08 11:07:55 +0200

    fix query - lint - debug options - added dotenv

--- etuutt.py
+++ etuutt.py
@@ -1,15 +1,19 @@
+import os
+
 from sqlalchemy import create_engine, text
 import phpserialize
 from os import environ
 
-def extract_data():
-  engine = create_engine(environ.get('SOURCE_DATABASE'), echo=True, future=True)
 
-  with engine.connect() as connection:
-    rows = connection.execute(text("SELECT id, login, semestersHistory FROM etu_users WHERE semestersHistory != 'a:0:{}'"))
-    
-    for row in rows:
-      yield {
-        'id': row.id,
-        'history': phpserialize.loads(bytearray(row.semestersHistory, 'utf8'), decode_strings=True)
-      }
+def extract_data():
+    engine = create_engine(environ.get('SOURCE_DATABASE'), echo=("DEBUG" in os.environ), future=True)
+
+    with engine.connect() as connection:
+        rows = connection.execute(
+            text("SELECT id, login, semestersHistory FROM etu_users WHERE semestersHistory NOT IN ('a:0:{}', '')"))
+
+        for row in rows:
+            yield {
+                'id': row.id,
+                'history': phpserialize.loads(bytearray(row.semestersHistory, 'utf8'), decode_strings=True)
+            }
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -5,21 +11,23 @@ from sqlalchemy.orm import sessionmaker
 import etuutt
 from os import environ
 
-engine = create_engine(environ.get('DESTINATION_DATABASE'), echo=True, future=True)
+engine = create_engine(environ.get('DESTINATION_DATABASE'), echo=("DEBUG" in os.environ), future=True)
 
 Base = declarative_base()
 
-class Mapping(Base):
-  __tablename__ = 'mapping'
 
-  id = Column(Integer, primary_key=True)
-  user_id = Column(Integer)
-  ue = Column(String(64))
-  semester = Column(String(64))
-  branch = Column(String(64))
-  level = Column(Integer)
-  faculty = Column(String(64))
-  formation = Column(String(64))
+class Mapping(Base):
+    __tablename__ = 'mapping'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)
+    ue = Column(String(64))
+    semester = Column(String(64))
+    branch = Column(String(64))
+    level = Column(Integer)
+    faculty = Column(String(64))
+    formation = Column(String(64))
+
 
 Mapping.__table__.drop(engine, checkfirst=True)
 Base.metadata.create_all(engine)
@@ -27,21 +35,22 @@ Base.metadata.create_all(engine)
 session = sessionmaker(bind=engine)()
 
 for user in etuutt.extract_data():
-  for semesterId, semester in user['history'].items():
-    for ue in semester['uvs'].values():
-      # The space for Autditor Libre is important (in the data it's like this)
-      if (ue 
-        and semester['formation'] not in [None, 'Nc', 'Doctorat', 'Auditeur libre ', 'These']
-        and semester['niveau'] not in ['2I1']):
-        print(f'{user["id"]} - {semesterId} - {ue}')
-        entity = Mapping(
-          user_id = user['id'], 
-          ue = ue, 
-          semester = semesterId, 
-          branch = semester.get('branch', None), 
-          level = semester['niveau'],
-          faculty = semester.get('filiere', None),
-          formation = semester['formation'])
-        session.add(entity)
-  
+    for semesterId, semester in user['history'].items():
+        for ue in semester['uvs'].values():
+            # The space for Autditor Libre is important (in the data it's like this)
+            if (ue
+                    and semester['formation'] not in [None, 'Nc', 'Doctorat', 'Auditeur libre ', 'These']
+                    and semester['niveau'] not in ['2I1']):
+                if environ.get("DEBUG"):
+                    print(f'{user["id"]} - {semesterId} - {ue}')
+                entity = Mapping(
+                    user_id=user['id'],
+                    ue=ue,
+                    semester=semesterId,
+                    branch=semester.get('branch', None),
+                    level=semester['niveau'],
+                    faculty=semester.get('filiere', None),
+                    formation=semester['formation'])
+                session.add(entity)
+
 session.commit()
